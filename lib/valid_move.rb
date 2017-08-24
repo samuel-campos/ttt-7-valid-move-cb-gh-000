d33500f5d7572a67906b2cae1f1e31a74e62bdc7
Sebastian Campos <21camposs@ptsdwebapps.com>
2017-08-24 02:44:56 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,7 +9,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if position_taken? == false && "#{index}".between?(0,8)
+  if position_taken? == false && index.between?(0,8)
     return true
   else
     return false
